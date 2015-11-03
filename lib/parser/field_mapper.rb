1fbd6a1f9ca0f58bc2d7fbd0fa0793f4bb705693
Gabriel De La O <matador.gabe@gmail.com>
2015-11-03 17:59:03 -0600

    Initial Commit.

---
+++ lib/parser/field_mapper.rb
@@ -0,0 +1,10 @@
+# Public: Various methods useful for performing mathematical operations.
+# All methods are module methods and should be called on the Math module.
+#
+# Examples
+#
+#   Math.square_root(9)
+#   # => 3
+class FieldMapper
+  attr_accessor :type
+end
---
+++ lib/parser/person.rb
@@ -0,0 +1,3 @@
+class Person
+  attr_accessor :first_name, :last_name, :gender, :favorite_color, :date_of_birth  
+end
