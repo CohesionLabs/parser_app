1b6aaeb6b57579f50b2a333a4fb7042a85c0e788
Gabriel De La O <matador.gabe@gmail.com>
2015-11-03 20:06:30 -0600

    Parsing and array of people based on a space delimiter and returning the string representation of the data in the correct order and sorted by the desired properties.

--- bin/parse.rb
+++ bin/parse.rb
@@ -4,13 +4,29 @@ require '../lib/parser/person'
 
 item_arr = []
 
+# Sort first, then return string representation.
+
 f = File.open("../space.txt","r")
 
 f.each_line { |line| 
 	
-	# Call the data/field mapper to hydrate the Person class.
+	#split_line = line.split(/\s/)
 	_person = FieldMapper.new(line)
-	
-	puts _person.get_mapped_data.representational_string
-	
+	#puts _person.get_mapped_data
+	item_arr.push(_person.get_mapped_data)	
 }
+
+# By Gender, then last name.
+item_arr.sort!{ |a,b|  (a.gender == b.gender) ? a.last_name <=> b.last_name : a.gender <=> b.gender }
+
+#puts item_arr.to_s
+
+item_arr.collect!{ |p| "#{p.to_s}" }
+
+puts item_arr.to_s
+
+## Do this after sorting. 
+# Call the data/field mapper to hydrate the Person class.
+	#_person = FieldMapper.new(line)
+	
+	#puts _person.get_mapped_data
--- lib/parser/person.rb
+++ lib/parser/person.rb
@@ -1,17 +1,23 @@
+require 'date'
 class Person
   attr_accessor :first_name, :last_name, :gender, :favorite_color, :date_of_birth 
   
   #Returns a string representation of the model in the desired format.
   def representational_string
-  	"#{@first_name} #{@last_name} #{gender} #{@date_of_birth} #{@favorite_color}"
+  	"#{@first_name} #{@last_name} #{gender} #{date_of_birth} #{@favorite_color}"
   end
   
   def gender
   	@gender = 'Female' if @gender[0].to_s.downcase == 'f'
   end
   
-  #def to_s
-  #	""
-  #end
+  def date_of_birth
+	d = DateTime.parse(@date_of_birth).strftime("%-m/%-d/%Y")
+  	@date_of_birth = d
+  end
+  
+  def to_s
+    "#{@first_name} #{@last_name} #{gender} #{date_of_birth} #{@favorite_color}"
+  end
    
 end
